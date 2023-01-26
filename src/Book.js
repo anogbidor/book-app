e6f6017962ecef716fe861cb2c9bf45118670ef2
Abraham <homenoffice247@gmail.com>
2023-01-26 13:41:26 +0300

    add a doing stuff message

--- src/Book.js
+++ src/Book.js
@@ -4,6 +4,7 @@ export const Book = (props) => {
   return (
     <article className='book'>
       <img src={img} alt={title} />
+      <p>Just doing some stuff</p>
       <h2>{title} </h2>
       <h4>{author}</h4>
       <span className='number'>#{number + 1}</span>
